8641db796b7212966110054342e73f14ee3cf1dc
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-09-01 22:40:08 -0400

    refactor: update main application class for enhanced configuration

--- backend/src/main/java/com/mauriexchange/code/BackendApplication.java
+++ backend/src/main/java/com/mauriexchange/code/BackendApplication.java
@@ -1,14 +1,18 @@
 package com.mauriexchange.code;
 
+// import com.mauriexchange.code.controller.DetailedCurrencyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = {
     DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class
+    HibernateJpaAutoConfiguration.class,
+    // DetailedCurrencyController.class
 })
+@EnableConfigurationProperties
 public class BackendApplication {
 
     public static void main(String[] args) {
